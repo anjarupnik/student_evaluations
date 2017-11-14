0f2373fde4aaf099cf2726077cf5a3697900e422
anjarupnik <anjarupnik@hotmail.com>
2017-11-14 10:18:03 +0100

    sign in

--- src/components/ui/Navigation.js
+++ src/components/ui/Navigation.js
@@ -5,25 +5,46 @@ import { push } from 'react-router-redux'
 import AppBar from 'material-ui/AppBar'
 import IconButton from 'material-ui/IconButton'
 import FlatButton from 'material-ui/FlatButton'
+import signOut from '../../actions/user/sign-out'
 
 const TITLE = 'Student Evaluations'
 
 class Navigation extends PureComponent {
+  static propTypes = {
+    signedIn: PropTypes.bool.isRequired,
+    push: PropTypes.func.isRequired,
+    signOut: PropTypes.func.isRequired,
+  }
 
+  signOut = (event) => {
+    event.preventDefault()
+    this.props.signOut()
+  }
+
+  signUp = () => {
+    this.props.push('/sign-up')
+  }
+
+  goHome = () => {
+    this.props.push('/')
+  }
 
   render() {
-
+    const { signedIn } = this.props
     return (
       <AppBar
         title={TITLE}
         iconElementLeft={<IconButton onClick={this.goHome}><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M5 13.18v4L12 21l7-3.82v-4L12 17l-7-3.82zM12 3L1 9l11 6 9-4.91V17h2V9L12 3z"/></svg>
           </IconButton>}
-
+        iconElementRight={signedIn ?
+           <FlatButton label="Sign out" onClick={this.signOut.bind(this)} /> : null}
       />
     )
   }
 }
 
+const mapStateToProps = ({ currentUser }) => ({
+  signedIn: (!!currentUser && !!currentUser._id)
+})
 
-
-export default (Navigation)
+export default connect(mapStateToProps, { push, signOut })(Navigation)
--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react'
 import { Route } from 'react-router-dom'
 
-import RecipesContainer from './recipes/RecipesContainer'
+import {
+  SignIn,
+  SignUp
+} from './containers'
 
 export default class Routes extends Component {
   render() {
     return (
       <div>
-        <Route exact path="/"  />
+        <Route exact path="/" />
+        <Route path="/sign-in" component={SignIn} />
+        <Route path="/sign-up" component={SignUp} />
       </div>
     )
   }
